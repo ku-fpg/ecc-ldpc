fb5123ed5c428b2f2fa93d17fd090cde39afa72a
Andy Gill <andygill@ku.edu>
2013-11-30 20:52:55 -0600

    Implemented LDPC.
    Found 3 bugs in spec; all bad transliterations.

--- src/ECC/Code/LDPC/Reference.hs
+++ src/ECC/Code/LDPC/Reference.hs
@@ -11,15 +11,10 @@ import Data.Matrix
 import qualified Data.Vector as V
 import Data.Alist
 
+import Debug.Trace
+
 type M a = Matrix a
 type V a = V.Vector a
-
-{-
-import Data.Array.Matrix
-import Data.Bit
-import Data.Array
-import ECC
--}
 
 code :: Code
 code = Code ["ldpc/reference/<matrix-name>/<max-rounds>"]
@@ -33,8 +28,8 @@ mkLDPC codeName maxI = do
    h :: H <- readAlist ("codes/" ++ codeName ++ ".H")
    return $ ECC
         { name     = "ldpc/reference/"
-        , encode   = return . encoder g
-        , decode   = return . (,True) . fmap mkBit . fmap (>= 0)
+        , encode   = return . V.toList . encoder g . V.fromList
+        , decode   = return . (,True) . take (nrows g) . V.toList . ldpc 128 h . V.fromList
         , message_length  = nrows g
         , codeword_length =  ncols g
         }
@@ -42,39 +37,53 @@ mkLDPC codeName maxI = do
 type G = M Bit
 type H = M Bit
 
-encoder :: G -> [Bit] -> [Bit]
-encoder g v = V.toList (getRow 1 (multStd (rowVector (V.fromList v)) g))
+encoder :: G -> V Bit -> V Bit
+encoder g v = getRow 1 (multStd (rowVector v) g)
 
-{-
-ldpc :: forall d. (Floating d, Ord d) => Int -> M Bit -> V d -> V Bit
+---------------------------------------------------------------------
+
+ldpc :: Int -> M Bit -> V Double -> V Bit
 ldpc maxIterations a orig_lam = fmap hard $ loop 0 orig_ne orig_lam
   where
-    orig_ne :: M d
+    orig_ne :: M Double
     orig_ne = fmap (const 0) a
 
-    loop :: Int -> M d -> V d -> V d
+    loop :: Int -> M Double -> V Double -> V Double
     loop !n ne lam
-        | all (== 0) (elems ans)       = lam
+--        | traceShow ("loop",n,fmap round ne,lam) False   = undefined
+        | V.all (== 0) ans             = lam
         | n >= maxIterations           = orig_lam
         | otherwise                    = loop (n+1) ne' lam'
       where
         c_hat :: V Bit
         c_hat = fmap hard lam
 
-        ans :: M Bit
-        ans = a `mm` columnM c_hat
+        -- was bug here: needed to getCol, not getRow (which was a single element)
+        ans :: V Bit
+        ans = getCol 1 (a `multStd` colVector c_hat)
 
-        ne' :: M d
-        ne' = ne // [ ((m,n), -2 * atanh (product
-                         [ tanh (- ((lam ! j - ne ! (m,j)) / 2))
-                         | j <- indices lam
-                         , j /= n
-                         , a ! (m,j) == 1
-                         ]))
-                    | (m,n) <- indices ne
-                    , a ! (m,n) == 1
-                    ]
+        -- was bug here: V's start at index 0, not 1
+        ne' :: M Double
+        ne' = matrix (nrows orig_ne) (ncols orig_ne) $ \ (m,n) ->
+                if a ! (m,n) == 1
+                then
+                    -2 * atanh (product
+                        [ tanh (- ((lam V.! (j-1) - ne ! (m,j)) / 2))
+                        | j <- [1 .. V.length orig_lam]
+                        , j /= n
+                        , a ! (m,j) == 1
+                        ])
+                else 0
 
-        lam' :: V d
-        lam' = accum (+) orig_lam [ (n,a) | ((_,n),a) <- assocs ne' ]
--}
+        -- Was bug here: needed to add the orig_lam
+        lam' :: V Double
+        lam' = V.fromList [ V.foldr (+) (orig_lam V.! (j - 1)) (getCol j ne')
+                          | j <- [1 .. V.length lam]
+                          ]
+
+
+crash = do
+        h <- readAlist ("codes/moon.7.13.H")
+        return $ ldpc 128 h v0
+
+v0 = V.fromList $ [10.31704008699972,-5.762379657732054,10.71923587403389,8.69474407787769,12.403269631011147,-4.8140434138889425,1.096112697392211,-7.578778502331838,5.607400012378008,-7.687307594577467e-2,12.161984232264581,10.392572265154772,-5.1354846290270215,6.046205353391934,10.257982632106899,15.459395584777264,-5.998393431786397,12.594442761269947,1.387024485274938,5.832772771282147]
