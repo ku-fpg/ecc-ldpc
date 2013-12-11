1480f8450b771143b14c6c95da9bb3196320794e
Andy Gill <andygill@ku.edu>
2013-12-10 19:55:50 -0600

    Adding MinSum2 support.

--- src/ECC/Code/LDPC/Utils.hs
+++ src/ECC/Code/LDPC/Utils.hs
@@ -7,6 +7,7 @@ import ECC.Types
 import ECC.Puncture
 import Data.BitMatrix.Loader
 import Data.Char (isDigit)
+import Data.Semigroup
 
 mkLDPC :: (MatrixLoader g, MatrixLoader h)
        => String -> String -> Int
@@ -46,3 +47,31 @@ atanh' :: RealFloat a => a -> a
 atanh' x | isInfinite y = signum x * 18.714973875118524
          | otherwise    = y
  where y = atanh x
+
+
+newtype MinSum a = MinSum a
+        deriving Show
+{-# INLINE metric #-}
+metric a b = min (abs a) (abs b) * signum a * signum b
+
+instance (Ord a, Num a) => Semigroup (MinSum a) where
+        {-# INLINE (<>) #-}
+        (MinSum a) <> (MinSum b) = MinSum $ metric a b
+
+data MinSum2 a = MinSum2 (MinSum a) Int (Maybe (MinSum a))
+        deriving Show
+
+instance  (Ord a, Num a) => Semigroup (MinSum2 a) where
+        (MinSum2 a1@(MinSum v1) i1 b1) <> (MinSum2 a2@(MinSum v2) i2 b2)
+                | abs v1 < abs v2 = MinSum2 (a1 <> a2) i1 (b1 <> Just a2)
+                | otherwise       = MinSum2 (a1 <> a2) i2 (Just a1 <> b2 )
+
+omit :: (Num a) => MinSum2 a -> Int -> a
+omit (MinSum2 (MinSum a) i (Just (MinSum b))) j
+        | i == j    = a         -- still needs sign adjust
+        | otherwise = abs b     -- also needs sign adjust
+
+
+
+
+
