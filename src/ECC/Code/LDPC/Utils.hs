f403a05882be196e1da8cc90a9f460e27e5ff403
David Young <roboguy06@gmail.com>
2017-05-17 15:18:44 -0500

    Compatibility with parametrized ECC

--- src/ECC/Code/LDPC/Utils.hs
+++ src/ECC/Code/LDPC/Utils.hs
@@ -13,7 +13,7 @@ mkLDPC :: (MatrixLoader g, MatrixLoader h)
        => String -> String -> Int
        -> (g -> [Bit] -> IO [Bit])
        -> (h -> Int -> [Double] -> IO [Bit])
-       -> IO ECC
+       -> IO (ECC IO)
 mkLDPC prefix codeName maxI encoder decoder = do
    g <- loadMatrix (codeName ++ "/G")   -- with G, we prepend the identity
    print (getNRows g, getNCols g)
