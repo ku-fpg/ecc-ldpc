50940c81e208f1f19da6cac2ea81625c89162e11
David Young <dyoung@ittc.ku.edu>
2017-07-10 23:40:18 -0500

    Started basics of CUDA implementation

--- cudabits/cached_mult.cu
+++ cudabits/cached_mult.cu
@@ -1,6 +1,17 @@
-extern "C" __global__ void ldpc(double* mLet, double* offsets, int rowCount, int colCount, int maxIterations, double* orig_lam, int* result) {
-  for (int i = 0; i < colCount; ++i) {
-    result[i] = 0;
+extern "C" __global__ void ldpc(double* mLet, double* offsets, int rowCount, int colCount, int maxIterations, double* orig_lam, int orig_lam_len, int* result) {
+  /* for (int i = 0; i < orig_lam_len; ++i) { */
+  /*   result[i] = 0; */
+  /* } */
+
+  int iters;
+  for (iters = 0; iters < maxIterations; ++iters) {
+
+  }
+
+  if (iters > 0) {
+    for (int i = 0; i < orig_lam_len; ++i) {
+      result[i] = orig_lam[i] > 0;
+    }
   }
 }
 
