30ba2b42379eb96e81065e67b0fade5639496223
David Young <dyoung@ittc.ku.edu>
2017-08-09 12:26:59 -0500

    Added (untested) matrix file converters. Also (probably) sped up compilation in .cabal

---
+++ utilities/ExpMatrix/Main.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE PatternSynonyms #-}
+
+module Main (main) where
+
+import Data.Matrix
+import System.Environment
+
+import System.FilePath.Posix (splitExtension)
+
+import Data.Matrix.Matlab
+
+pattern Extension = ".qx"
+
+main :: IO ()
+main = do
+  args <- getArgs
+
+  case args of
+    [fileName]
+      | (withoutExt, Extension) <- splitExtension fileName -> do
+          fileContents <- getContents
+
+          let Matlab mat = read fileContents :: Matlab Integer
+
+          print (Matlab (fmap (^2) mat))
+      | otherwise ->
+          error ("Wrong extension (expected " ++ Extension ++ ")")
+
+    _ -> error "Wrong number of arguments. Expected 1."
+
---
+++ utilities/Glue64/Main.hs
@@ -0,0 +1,44 @@
+-- | Glue (row) consecutive unsigned 64-bit integers into 128-bit integers.
+{-# LANGUAGE PatternSynonyms #-}
+
+module Main (main) where
+
+import Data.Word (Word64)
+
+import Data.Matrix
+import System.Environment
+
+import System.FilePath.Posix (splitExtension)
+
+import Data.Matrix.Matlab
+
+import Data.Bits
+
+pattern Extension = ".q64"
+
+rowGlue :: [Word64] -> [Integer]
+rowGlue (x:y:rest) = ((fromIntegral x `shiftL` 64) .|. fromIntegral y) : rowGlue rest
+rowGlue []         = []
+rowGlue _          = error "Odd number of items in row."
+
+main :: IO ()
+main = do
+  args <- getArgs
+
+  case args of
+    [fileName]
+      | (withoutExt, Extension) <- splitExtension fileName -> do
+          fileContents <- getContents
+
+          let Matlab mat = read fileContents :: Matlab Word64
+
+              mat'Lists  = rowGlue <$> toLists mat
+
+          print (fromLists mat'Lists)
+
+      | otherwise ->
+          error ("Wrong extension (expected " ++ Extension ++ ")")
+
+    _ -> error "Wrong number of arguments. Expected 1."
+
+
