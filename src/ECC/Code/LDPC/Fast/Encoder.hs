26cdd195e9bc2cf7c6e2bc39edc151d57f50fcc9
David Young <roboguy06@gmail.com>
2017-08-10 06:26:29 -0500

    Generalized fast encoder somewhat (using external large integer library)

--- src/ECC/Code/LDPC/Fast/Encoder.hs
+++ src/ECC/Code/LDPC/Fast/Encoder.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE BangPatterns, RankNTypes, GeneralizedNewtypeDeriving, ScopedTypeVariables #-}
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TupleSections, TypeApplications #-}
 module ECC.Code.LDPC.Fast.Encoder where
 
 -- Fast implementations of LDPC encoder.
@@ -16,40 +16,51 @@ import Data.Monoid
 import Debug.Trace
 import qualified Data.Matrix.QuasiCyclic as Q
 import Data.Word
+import Data.DoubleWord
 import Data.Bits
+import Data.Proxy
 
 type M a = Matrix a
 type V a = U.Vector a
 
 encoder :: Q.QuasiCyclic Integer -> Rate -> U.Vector Bool -> U.Vector Bool
-encoder g@(Q.QuasiCyclic sz m) = 
+encoder g@(Q.QuasiCyclic sz m) =
     case sz of
-      32 -> let m32 :: Matrix Word32
-                m32 = fmap fromIntegral m 
-            in \ r v -> 
-                     let v' :: U.Vector Word32
-                         v' = U.generate (nrows m) $ \ row -> 
-                                 foldr (.|.) zeroBits [ if (v U.! (row * sz + i))
-                                                        then bit i
-                                                        else zeroBits
-                                                      | i <- take sz [0..]
-                                                      ]
-                         res :: U.Vector Word32
-                         res = U.generate (ncols m) $ \ col ->
-                                     foldr xor zeroBits [ mulQWord32 (v' U.! row) (m32 M.! (row+1,col+1))
-                                                        | row <- take (nrows m) [0..]
-                                                        ]
-                                 
-                     in U.generate (sz * ncols m) $ \ col -> (res U.! (col `div` sz)) `testBit` (col `mod` sz)
-      _  -> error $ "unsupported size for fast encoder : " ++ show sz
-
-
-mulQWord32 :: Word32 -> Word32 -> Word32
-mulQWord32 w1 w2 = go w2 0
+      32  -> go (Proxy @Word32)
+      64  -> go (Proxy @Word64)
+      128 -> go (Proxy @Word128)
+      256 -> go (Proxy @Word256)
+      _   -> error $ "unsupported size for fast encoder : " ++ show sz
   where
-    go !w 32 = 0
-    go !w !n | w1 `testBit` n = w `xor` go (w `rotateL` 1) (n+1)
-             | otherwise      =         go (w `rotateL` 1) (n+1)
+    go :: forall w. (Num w, FiniteBits w) =>
+      Proxy w -> Rate -> U.Vector Bool -> U.Vector Bool
+    go Proxy =
+      let m32 :: Matrix w
+          m32 = fmap fromIntegral m
+      in \ r v ->
+               let v' :: V.Vector w
+                   v' = V.generate (nrows m) $ \ row ->
+                           foldr (.|.) zeroBits [ if (v U.! (row * sz + i))
+                                                  then bit i
+                                                  else zeroBits
+                                                | i <- take sz [0..]
+                                                ]
+                   res :: V.Vector w
+                   res = V.generate (ncols m) $ \ col ->
+                               foldr xor zeroBits [ mulWord (v' V.! row) (m32 M.! (row+1,col+1))
+                                                  | row <- take (nrows m) [0..]
+                                                  ]
+
+               in
+               V.convert $ V.generate (sz * ncols m) $ \ col -> (res V.! (col `div` sz)) `testBit` (col `mod` sz)
+
+
+mulWord :: (Num a, FiniteBits a) => a -> a -> a
+mulWord w1 w2 = go w2 0
+  where
+    go !w !n | n == finiteBitSize w1 = 0
+    go !w !n | w1 `testBit` n        = w `xor` go (w `rotateL` 1) (n+1)
+             | otherwise             = go (w `rotateL` 1) (n+1)
 
 -- A reference implementation, to testing arbitrary finite bit width
 refMulQWord32 :: Bits w => w -> w -> w
